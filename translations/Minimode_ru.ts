74d047d40c61f47a323e1e7e2498f7f55aa9e037
Ilya87 <yast4ik@yahoo.com>
2016-02-12 22:33:47 +0300

    Add Russian translation

--- translations/Minimode_ru.ts
+++ translations/Minimode_ru.ts
@@ -5,18 +5,29 @@
     <name>MiniMode</name>
     <message>
         <source>&lt;No track&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;Нет дорожки&gt;</translation>
     </message>
 </context>
 <context>
     <name>MiniModeConfigPage</name>
     <message>
         <source>Tribute to Winamp</source>
-        <translation type="unfinished"></translation>
+        <translation>Отдаю должное Winamp</translation>
     </message>
     <message>
         <source>Use Winamp theme</source>
-        <translation type="unfinished"></translation>
+        <translation>Использовать тему Winamp</translation>
+    </message>
+</context>
+<context>
+    <name>QCoreApplication</name>
+    <message>
+        <source>There are more than 300 files to add to the %1 (%2 to add).</source>
+        <translation>Есть больше 300 файлов для добавления в %1 (%2, чтобы добавить).</translation>
+    </message>
+    <message>
+        <source>Are you sure you want to continue? This might take some time.</source>
+        <translation>Вы уверены, что хотите продолжить? Это может занять некоторое время.</translation>
     </message>
 </context>
 </TS>
